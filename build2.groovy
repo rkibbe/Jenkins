da68c68db81cb7d76cd67204f23ab4f20259b0f7
Rkibbe <randy.kibbe@cognosante.com>
2016-12-17 17:26:06 -0500

    new job

--- build2.groovy
+++ build2.groovy
@@ -1,4 +1,4 @@
-def project = 'Rkibbe/Jenkins'
+def project = 'rkibbe/jenkins'
 def branchApi = new URL("https://api.github.com/repos/${project}/branches")
 def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
 branches.each {
@@ -6,7 +6,10 @@ branches.each {
     def jobName = "${project}-${branchName}".replaceAll('/','-')
     job(jobName) {
         scm {
-            git("https://github.com/${project}.git", branchName)
+            git("git://github.com/${project}.git", branchName)
+        }
+        steps {
+            maven("test -Dproject.name=${project}/${branchName}")
         }
     }
 }
