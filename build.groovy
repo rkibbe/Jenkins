a158d1da4af43657ec99d9122ea2688c529a9ddd
Rkibbe <randy.kibbe@cognosante.com>
2016-12-19 18:51:13 -0500

    add builders

--- build.groovy
+++ build.groovy
@@ -10,9 +10,9 @@ job('DSL-codedeploy') {
     steps {
         msBuild {
             msBuildInstallation('MSBuild 1.8')
-            buildFile('dir1/build.proj')
-            args('check')
-            args('another')
+            buildFile('.\EagleEye.sln')
+            args('')
+            args('')
             passBuildVariables()
             continueOnBuildFailure()
             unstableIfWarnings()
@@ -21,6 +21,14 @@ job('DSL-codedeploy') {
         project / publishers / 'com.amazonaws.codedeploy.AWSCodeDeployPublisher' { 
              s3bucket 'dev-build-artifacts1' 
 			 applicationName 'Cerrs.EagleEye'
+			 region 'us-east-1'
+			 includes '**'
+			 excludes '*.csproj'
+			 subdirectory 'EagleEye'
+			 awsAccessKey 'AKIAJSJRXSVMYSRKY36Q'
+			 awsSecretKey '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'
+			 credentials 'awsAccessKey'
+			 deploymentMethod 'deploy'
          } 
      } 
 
